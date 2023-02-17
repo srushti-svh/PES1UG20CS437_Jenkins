8d750ba18138887b8905c7fb01a554928c269254
srushti-svh <79077323+srushti-svh@users.noreply.github.com>
2023-02-18 00:16:26 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "Hello, This is Srushti\n";
+  cout << "Hello, This is Srushti Hampannavar\n";
   cout << "I have successfully built and run PES1UG20CS437\n";
   return 0;
 }
